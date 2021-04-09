7e7ca63591d97aadb16a67d4b71928aae473f6d5
Aleksandr Ardanov <alex@air-aleksandr.lan>
2021-04-09 09:57:02 -0500

    added scrits

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,28 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/',(req, res, next) => {
+
+})
+
+router.get('/:id',(req, res, next) => {
+    
+})
+
+router.post('/',(req, res, next) => {
+    
+})
+
+router.put('/:id',(req, res, next) => {
+    
+})
+
+router.delete('/:id',(req, res, next) => {
+    
+})
+
+router.get('/:id/actions',(req, res, next) => {
+
+})
